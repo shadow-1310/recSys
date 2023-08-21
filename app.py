29f9fd9352fa286164bb386b2333970d461ecefb
Shekhar <shekharnath9@gmail.com>
2023-08-21 18:19:31 +0530

    added modification to the interface

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from recSys.pipeline.prediction import PredictionPipeline
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -39,5 +40,35 @@ def recommend_genre():
             rating = list(data['average_rating'].values)
             )
 
+
+@app.route('/content_based')
+def content_ui():
+    obj = PredictionPipeline()
+    path = obj.config.model_path.content_based.books
+    books = pd.read_csv(path)
+    books = list(books['title'].values)
+
+    return render_template('content_based_recommender.html',
+            options = books)
+
+@app.route('/content_based_recommender', methods=['post'])
+def content_recommender():
+    obj = PredictionPipeline()
+    path = obj.config.model_path.content_based.books
+    books = pd.read_csv(path)
+    books = list(books['title'].values)
+    user_input = request.form.get('user_input')
+    data = obj.content_based_recommender(user_input)
+
+    return render_template('content_based_recommender.html',
+            options = books,
+            title = list(data['title'].values),
+            author = list(data['authors'].values),
+            image = list(data['image_url'].values),
+            rating = list(data['average_rating'].values)
+            )
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+

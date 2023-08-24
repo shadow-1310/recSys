83f5ad366294df9a1e6e0412705b9d40f51c4002
Shekhar <shekharnath9@gmail.com>
2023-08-24 21:11:24 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def content_recommender():
             rating = list(data['average_rating'].values)
             )
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port= 8080, debug=True)
 
 
 
